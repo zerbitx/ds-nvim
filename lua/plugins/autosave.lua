f6eaccc03f8cbaf7e1f396753b2b64eac9fe3861
RyanSwanson <ryan.a.swanson@gmail.com>
2024-09-19 14:21:22 -0600

    Remove redundant config

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -3,7 +3,4 @@ return {
   cmd = "ASToggle",                         -- optional for lazy loading on command
   event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
   opts = { enabled = true },
-  -- keys = {
-    -- { "<leader>n", ":ASToggle<CR>", desc = "Toggle auto-save" },
-  -- },
 }
